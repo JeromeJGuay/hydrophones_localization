40e0c3b1026dc0d304dc09707577b1ad7605777f
Guay <Jerome.Guay@dfo-mpo.gc.ca>
2024-07-29 10:31:34 -0400

    update

---
+++ figures.m
@@ -0,0 +1,35 @@
+classdef figures
+    methods ( Static = true )
+        function pyramid_3d_solution(real, guess, solution)
+            labels = 1:size(real, 2);
+            figure
+            hold on
+           
+            figures.pyramid_3d_plot(real, 'b', labels);
+            figures.pyramid_3d_plot(guess,'k', labels);
+            figures.pyramid_3d_plot(solution, 'r', labels);
+
+            hold off
+            set(gca,'ZDir','reverse')
+            grid on
+            legend('Guess','', 'Real', '', 'Solution')
+            view(3)
+            daspect([1 1 1])
+            shg
+        end
+    end
+
+    methods (Static= true, Access= private)
+        
+        function pyramid_3d_plot(receivers, color, labels)
+            plot3(receivers(1,:),receivers(2,:),receivers(3,:), strcat(color,'.'),'MarkerSize',10)
+            trisurf(boundary(receivers',1),receivers(1,:)',receivers(2,:)',receivers(3,:)','FaceColor',color,'FaceAlpha',0.1)
+            for i=1:size(receivers,2)
+               text(receivers(1,i),receivers(2,i),receivers(3,i),['   ' ...
+                num2str(labels(i))],'HorizontalAlignment','left','FontSize',8);
+            end    
+        end
+    
+    end
+end
+
--- hydrophones_localization_test.m
+++ hydrophones_localization_test.m
@@ -1,188 +1,91 @@
-%% Step 4: 2 receivers N-pings locations
 clear all
 close all
 clc
 
-% 639
-% xs = [1258.825 1385.942 1308.191 1214.396 1439.01 1383.474];
-% ys = [748.713 739.447 867.322 822.844 854.348 919.213]; 
-
-%637
-xs = [477.613 414.672 340.623 550.427 494.891 381.35 ];
-ys = [164.939 283.547 202.005 255.749 346.558 138.994];
-
-zs = [5 5 5 5 5 5];
-ns = length(xs);
-
 %% Known parameters
-% set source location
-s = [xs(:).';ys(:).';zs(:).'];
 
 % sound-speed [m/s]
 c0 = 1500;
 
-%% Test Data
+% Sources Positions
+xs = [477.613 414.672 340.623 550.427 494.891 381.35 ];
+ys = [164.939 283.547 202.005 255.749 346.558 138.994];
+zs = [5 5 5 5 5 5];
+
+sources = [xs(:).';ys(:).';zs(:).'];
+Ns = size(sources, 2);
 
 % pyramid position
-pyramid_x = 444.291;
-pyramid_y = 233.51;
-pyramid_z =  30.21;
+pyramid_x = 444;
+pyramid_y = 233;
+pyramid_z =  30;
 
 % receiver locations
-xr = [-1 -0 1 0] + pyramid_x;
-yr = [-1 .5 -1 -0.5] + pyramid_y;
-zr = [0 0 0 -1] + pyramid_z;
+r_1_xyz = [-1  -1    0];
+r_2_xyz = [ 0  .5    0];
+r_3_xyz = [1   -1   0];
+r_4_xyz = [0  -.5  -1];
 
-% yr = [-1 .5 -1 -0.5] + pyramid_y + rand(1,4)*1-.5;
-% zr = [0 0 0 -1] + pyramid_z + rand(1,4)*1-.5;
-% xr = [-1 -0 1 0] + pyramid_x + rand(1,4)*1-.5;
-
-nr = length(xr);
-
-r = [xr; yr; zr];
+receivers = ([r_1_xyz; r_2_xyz; r_3_xyz; r_4_xyz] + [pyramid_x pyramid_y pyramid_z])';
+Nr = size(receivers, 2);
 
 % synthetic travel time and delays
-tau_mes = sqrt( (xr(:)-xs(:).').^2  + (yr(:)-ys(:).').^2 + (zr(:)-zs(:).').^2 ) / c0;
+xr = receivers(1,:);
+yr = receivers(2,:);
+zr = receivers(3,:);
 
-dtau_mes = NaN(nr,nr,ns);
+tau_meas = sqrt( (xr(:)-xs(:).').^2  + (yr(:)-ys(:).').^2 + (zr(:)-zs(:).').^2 ) / c0;
 
-for i_e = 1:ns
-    dtau_mes(:,:,i_e) = tau_mes(:,i_e)-tau_mes(:,i_e).';
+delays_meas = NaN(Nr,Nr,Ns);
+for i_e = 1:Ns
+    delays_meas(:,:,i_e) = tau_meas(:,i_e) - tau_meas(:,i_e).';
 end
-
-
 
 %% Variables Initialization
-% receiver locations
+% receiver guess location
 
-r1_xyz = [-1  -1    0];
-r2_xyz = [ 0  .5    0];
-r3_xyz = [1   -1   0];
-r4_xyz = [0  -.5  -1];
+r0_1_xyz = [-1  -1    0];
+r0_2_xyz = [ 0  .5    0];
+r0_3_xyz = [1   -1   0];
+r0_4_xyz = [0  -.5  -1];
 
-r0 = ([r1_xyz; r2_xyz; r3_xyz; r4_xyz] + [pyramid_x pyramid_y pyramid_z])';
+r0 = ([r0_1_xyz; r0_2_xyz; r0_3_xyz; r0_4_xyz] + [pyramid_x pyramid_y pyramid_z])';
 
-%% Initial guess is real position with noise
+%% Solving
+% None Linear constrainte (distance between hydrophones)
+%max_distance = 3;
+%nonlcon = @(X) nonlinear_constraint.max_distance(X, max_distance);
 
-%r0 = r + (rand(3,4)*2 -1) ;
+cfun = @(X) lowcost_functions.R(X, sources, c0, delays_meas);
 
+% z_range = -20:20;
+delta_zr0 = -10:0.5:10;
 
-%% swap initial receivers position
-r0 = r0(:,[1 3 4 2]);
+mean_positions = zeros(size(delta_zr0, 2),3);
 
+for i=1:length(delta_zr0)
+    r00 = r0 + [1;1;1] * delta_zr0(i);
+    xsol = fmincon(cfun,r00);
+    mean_positions(i,:) = mean(xsol, 2);
+end
 
-%% None Linear constrainte (distance between hydrophones)
-max_distance = 3;
-nonlcon = @(X) nonlinear_constraint.max_distance(X, max_distance);
-
-
-%% optimize
-
-% from percent of the period of the max freq
-fmax = 256e3/2;
-Tmin = 1/fmax;
-tolfun = Tmin/100;
-
-%cfun = @(X) loccostfun2(s,X,c0,dtau_mes);
-
-% cfun = @(X) lowcost_R(X, s, c0, dtau_mes);
-cfun = @(X) lowcost_functions.R(X, s, c0, dtau_mes);
-
-
-%xsol = fmincon(cfun,r0,[],[],DD,dd);
-%xsol = fmincon(cfun,r0,[],[],[],[],[], [], nonlcon);
-xsol = fmincon(cfun,r0);
-
-%% PLot
-receivers_label = [1 2 3 4];
+%% Figures
 figure
-hold on
+plot(delta_zr0, mean_positions(:,1) - pyramid_x)
+xlabel('\Delta Zr_0')
+ylabel('solution x diff')
 
-% Ping Location
-% plot3(xs,ys,zs,'kp','MarkerFaceColor','k')
+figure
+plot(delta_zr0, mean_positions(:,2) - pyramid_y)
+xlabel('\Delta Zr_0')
+ylabel('solution y diff')
 
-% Initial Guess
-P = r0;
-plot3(P(1,:),P(2,:),P(3,:),'k.','MarkerSize',10)
-trisurf(boundary(P',1),P(1,:)',P(2,:)',P(3,:)','FaceColor','black','FaceAlpha',0.1)
-for i=1:nr
-   text(P(1,i),P(2,i),P(3,i),['   ' ...
-    num2str(receivers_label(i))],'HorizontalAlignment','left','FontSize',8);
-end
-
-% real position
-P = r;
-plot3(P(1,:),P(2,:),P(3,:),'blue.','MarkerSize',10)
-trisurf(boundary(P',1),P(1,:)',P(2,:)',P(3,:)','FaceColor','blue','FaceAlpha',0.1)
-for i=1:nr
-   text(P(1,i),P(2,i),P(3,i),['   ' ...
-    num2str(receivers_label(i))],'HorizontalAlignment','left','FontSize',8);
-end
+figure
+plot(delta_zr0, mean_positions(:,3) - pyramid_z)
+xlabel('\Delta Zr_0')
+ylabel('\Delta Z')
+%figures.pyramid_3d(r0, receivers, xsol)
 
 
-% Solutions
-P = xsol;
-plot3(P(1,:),P(2,:),P(3,:),'r.','MarkerSize',10)
-trisurf(boundary(P',1),P(1,:)',P(2,:)',P(3,:)','FaceColor','red','FaceAlpha',0.1)
-for i=1:nr
-   text(P(1,i),P(2,i),P(3,i),['   ' ...
-    num2str(receivers_label(i))],'HorizontalAlignment','left','FontSize',8);
-end
-hold off
-set(gca,'ZDir','reverse')
-grid on
-% legend('Sources', 'Guess', 'Solution')
-view(3)
-daspect([1 1 1])
-shg
-
-%% test
-dr1 = sqrt(sum((xsol(:,1) - xsol(:,[1 2 3 4])).^2));
-dr2 = sqrt(sum((xsol(:,2) - xsol(:,[1 2 3 4])).^2));
-dr3 = sqrt(sum((xsol(:,3) - xsol(:,[1 2 3 4])).^2));
-dr4 = sqrt(sum((xsol(:,4) - xsol(:,[1 2 3 4])).^2));
-
-dr = [dr1; dr2; dr3; dr4];
-
-disp('Distance between hydrophone')
-disp('      1     2     3     4')
-fprintf('%d: %.2f  %.2f  %.2f  %.2f\n', [(1:4)', dr]')
-
-% distance between hydrophone r < 2.5, c0 = 1465;
-%       1     2     3     4
-% 1: 0.00  1.93  2.03  1.53
-% 2: 1.93  0.00  2.03  1.81
-% 3: 2.03  2.03  0.00  1.76
-% 4: 1.53  1.81  1.76  0.00
-
-% distance between hydrophone Aucune contrainte, c0 = 1465;
-%       1     2     3     4
-% 1: 0.00  1.93  2.03  1.54
-% 2: 1.93  0.00  2.03  1.82
-% 3: 2.03  2.03  0.00  1.79
-% 4: 1.54  1.82  1.79  0.00
-
-% Distance between hydrophone c0=1500
-%       1     2     3     4
-% 1: 0.00  1.98  2.08  1.56
-% 2: 1.98  0.00  2.08  1.85
-% 3: 2.08  2.08  0.00  1.80
-% 4: 1.56  1.85  1.80  0.00
-
-% xsol(:, 1) c0=1500
-%   436.4800
-%   213.5600
-%    34.3800
-
-% % Distance between hydrophone c0=1450
-%       1     2     3     4
-% 1: 0.00  1.91  2.01  1.51
-% 2: 1.91  0.00  2.01  1.79
-% 3: 2.01  2.01  0.00  1.75
-% 4: 1.51  1.79  1.75  0.00
-
-% xsol(:, 1) c0=1450
-%   436.5200
-%   213.5500
-%    34.3200
-
+%%
+tools.compute_distances_receivers_to_sources
